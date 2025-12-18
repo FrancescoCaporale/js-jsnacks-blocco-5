8df720ebe9bdb95e8d307db41f5228b611a3be62
FrancescoCaporale <frenk.caporale@gmail.com>
2025-12-18 16:12:22 +0100

    jsnack 5 filter

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,10 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// Creo un nuovo array con solo i numeri pari
+const evenNumbers = nums.filter(function(number) {
+  return number % 2 === 0;
+});
+
+console.log(evenNumbers)
