852704cc2d48a8b25a730f821fb5dc9bf779e10f
FrancescoCaporale <frenk.caporale@gmail.com>
2025-12-18 15:42:27 +0100

    jsnack3 map

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,10 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// Creo un nuovo array con tutti i numeri incrementati di 1
+const incrementedNumbers = numbers.map(function(number) {
+  return number + 1;
+});
+
+console.log(incrementedNumbers) 
